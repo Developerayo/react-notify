16b674aa8afbdfa9c0a825bbc09a04c0caa8b1b0
Shodipo Ayomide <shodipovi@gmail.com>
2019-04-11 16:24:49 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Noty from 'noty'
+
 import './App.css';
+import './noty.css'
+import './bootstrap-v4.css'
+import './animate.css'
+// import '../node/modules/noty/lib/themes/mint.css'
+
+// import '../node/modules/noty/lib/themes/bootstrap-v4.css'
 
 class App extends Component {
+  showSuccess(){
+    new Noty({
+      type:"success",
+      theme:"bootstrap-v4",
+      layout:"topLeft",
+      text:"Some Notification text...",
+      timeout:"3000"
+    }).show()
+  }
+ showError(){
+    new Noty({
+      type:"error",
+      theme:"bootstrap-v4",
+      layout:"topLeft",
+      text:"Some Notification text...",
+      timeout:"3000"
+    }).show()
+  }
+  showInfo(){
+    new Noty({
+      type:"info",
+      theme:"bootstrap-v4",
+      layout:"topLeft",
+      text:"Some Notification text...",
+      timeout:"3000"
+    }).show()
+  }
+  // showButtonBounce(){
+  // new Noty({
+  //   text: 'Some Notifcation text',
+  //   animation: {
+  //       open: 'animated bounceInRight', // Animate.css class names
+  //       close: 'animated bounceOutRight' // Animate.css class names
+  //   }
+  //   }).show();
+  // }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       
+      <button onClick={(e)=>this.showSuccess(e)}>Show Success Message</button>
+      <button onClick={(e)=>this.showError(e)}>Show Error Message</button>
+      <button onClick={(e)=>this.showInfo(e)}>Show Info Message</button>
+      {/* <button onClick={(e)=>this.showButtonBounce(e)}>Show Info Message</button> */}
       </div>
-    );
+    ); 
   }
 }
 
